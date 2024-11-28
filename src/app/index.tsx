198adddf554d780713d12e496ac3d1ee6a198c3f
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2024-11-28 21:33:05 +0200

    init expo project with dummy data

---
+++ src/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>AAAAAAAAA</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff",
+  },
+});
