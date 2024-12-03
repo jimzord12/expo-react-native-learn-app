66a7d1cf2474c98a26ff01c29c4eb64d949b8157
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2024-12-03 17:22:02 +0200

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+const Index = () => (
+  <View>
+    <Link href={"/(tabs)"}>
+      <Text>AAAAAAA</Text>
+    </Link>
+  </View>
+);
+
+export default Index;
+
+const styles = StyleSheet.create({});
